ed2895e4b9a93314d5bbaca2a08689d12543b97b
유동규 <kizs3515@naver.com>
2015-10-06 16:59:51 +0900

    10.6일 엑셀.파이썬
    ㅇㅇ

---
+++ 10.06엑셀/else.py
@@ -0,0 +1,12 @@
+for i in range(10):
+    print i,
+else:
+    print "EOP"
+
+a = 0
+while a < 10:
+    print a,
+    a += 1
+else:
+    print "EOP"
+    
---
+++ 10.06엑셀/while.py
@@ -0,0 +1,7 @@
+for i in range(0,5):
+    print i
+
+a = 1
+while a <10:
+    print a
+    a += 1
